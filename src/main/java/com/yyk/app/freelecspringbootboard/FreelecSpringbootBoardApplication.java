1210b437a8264e95788df2a70d16c4e8032130c4
YoungKyonYou <man.spride1324@gmail.com>
2021-08-13 16:24:56 +0900

    Initial commit

---
+++ src/main/java/com/yyk/app/freelecspringbootboard/FreelecSpringbootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.yyk.app.freelecspringbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringbootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringbootBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yyk/app/freelecspringbootboard/FreelecSpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyk.app.freelecspringbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
